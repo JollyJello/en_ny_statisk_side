2db50abaa35b199ed10cda232b28a231a6b9f288
Esther B <esth3734@stud.kea.dk>
2023-02-06 16:35:00 +0100

    v1 product færdigt

--- js/product.js
+++ js/product.js
@@ -1,10 +1,12 @@
 //https://kea-alt-del.dk/t7/api/products/1525
-fetch("https://kea-alt-del.dk/t7/api/products/1525");
-   .then((response)=>response.json())
-   .then((data) => showProduct(data))
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((response) => response.json())
+  .then((data) => showProduct(data));
 
-   function showProduct(product){
-    console.log(product);
-    document.querySelector(".purchaseBox h3").textContent = 
-    product.productdisplayname;
-   }
+function showProduct(product) {
+  console.log(product);
+  document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+  console.log(product);
+  document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+  document.querySelector("img").src = imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+}
