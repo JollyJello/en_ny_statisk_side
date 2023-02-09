8a806cea61db756b0d91d063275748ad0a5e5f36
Esther B <esth3734@stud.kea.dk>
2023-02-09 19:41:01 +0100

    update på alle sider

--- js/productlist.js
+++ js/productlist.js
@@ -18,10 +18,17 @@ function showProduct(product) {
 
   //ændre indhold
   copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(".subtle").textContent = product.articletype + " | " + product.brandname;
+  copy.querySelector("img").src = "https://kea-alt-del.dk/t7/images/webp/640/" + product.id + ".webp";
+  copy.querySelector("img").alt = product.productdisplayname;
+  copy.querySelector(".price").textContent = product.price + " DKK";
+  copy.querySelector(".discounted").textContent = product.discount;
+
   if (product.soldout) {
     //produktet er udsolgt
     copy.querySelector("article").classList.add("soldOut");
   }
+  copy.querySelector(".read-more").setAttribute("href", `product.html?id=${product.id}`);
 
   //appende
   document.querySelector("main").appendChild(copy);
@@ -33,7 +40,7 @@ function showProduct(product) {
             <h3>Blue T20 Indian Cricket Jersey</h3>
             <p class="subtle">Tshirts | Nike</p>
             <p class="price"><span>Prev.</span> DKK 1595,-</p>
-            <div class="discounted">
+            <div class="disxcounted">
                 <p>Now DKK 1560,-</p>
                 <p>-34%</p>
             </div>
@@ -43,31 +50,17 @@ function showProduct(product) {
 
 /*
 
-id	1165
+id	1164
 gender	"Men"
 category	"Apparel"
 subcategory	"Topwear"
 articletype	"Tshirts"
-basecolour	"Blue"
-season	"Summer"
-productionyear	2013
+season	"Winter"
+productionyear	2015
 usagetype	"Sports"
-productdisplayname	"Mean Team India Cricket Jersey"
-parsed	1
-soldout	0
-relid	1165
-price	2495
-discount	45
-variantname	"Authentic Jersey"
+productdisplayname	"Blue T20 Indian Cricket Jersey"
+price	1595
+discount	28
 brandname	"Nike"
-brandbio	"Nike, creating experiences for today’s athlete"
-brandimage	"http://assets.myntassets.com/v1/assets/banners/2015/6/26/1435317851398-23197-3chxv6.jpg"
-agegroup	"Adults-Men"
-colour1	"NA"
-colour2	"NA"
-fashiontype	"Fashion"
-materialcaredesc	"<p>Polyester<br />Machine wash</p>"
-sizefitdesc	null
-description	"<p>Blue jersey with a tipped collar, concealed, half buttoned placket, Nike print on the right chest, Team India print and applique on the front, short sleeves, tri-colour side panels, print at the back</p>"
-styledesc	"<p>Root for our country in style with this team India jersey from Nike. The Dri-fit fabric and panel"
+soldout	1
 */
